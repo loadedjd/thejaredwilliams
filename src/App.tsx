2108593640ae0def7ec672bacbed19512805adc8
Jared Williams <me@thejaredwilliams.com>
2024-08-25 17:42:05 -0400

    Stage config

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import packageJson from "../package.json";
 
 function App() {
   const version = packageJson.version;
+  const target = import.meta.env.VITE_TARGET;
 
   return (
     <>
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        Version {version}
+        Version {version} | Target {target}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
