f257ed1fd02e4ab7b0cf63a0a4b2122053388754
Jared Williams <me@thejaredwilliams.com>
2024-08-25 18:46:55 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        Version {version} | Target {target}
+        Versions {version} | Target {target}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
