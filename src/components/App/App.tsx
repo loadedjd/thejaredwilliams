285804ad4bd8a9ff60c4da08d713a59bf962dad4
Jared Williams <me@thejaredwilliams.com>
2024-08-25 17:35:25 -0400

    Routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,23 @@
-import reactLogo from "../..//assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import packageJson from "../../../package.json";
 
 function App() {
   const version = packageJson.version;
+  const target = import.meta.env.VITE_TARGET;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper">
+      <div className="pill-container">
+        <div className="pill">
+          <p className="pill-text">Version</p>
+          <p className="pill-value">{version}</p>
+        </div>
+        <div className="pill">
+          <p className="pill-text">Environment</p>
+          <p className="pill-value">{target}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        Version {version}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
