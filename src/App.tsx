aa892ee8e9da88538c597fdfa52944704c346c9e
Jared Williams <me@thejaredwilliams.com>
2024-08-26 09:22:06 -0400

    add confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import packageJson from "../package.json";
+import confetti from "canvas-confetti";
+import { useEffect, useMemo } from "react";
 
 function App() {
   const version = packageJson.version;
   const target = import.meta.env.VITE_TARGET;
 
+  useEffect(() => {
+    document.addEventListener("click", function (event) {
+      const x = event.clientX / window.innerWidth;
+      const y = event.clientY / window.innerHeight;
+
+      confetti({ origin: { x, y }, particleCount: 10, spread: 80 });
+    });
+  }, []);
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Jared</h1>
       <div className="card">
         Versions {version} | Target {target}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
